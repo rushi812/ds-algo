90a07e5ec17b50355bef9e5b27301341890dca57
Rushiraj Brahmbhatt <rbrahm812@gmail.com>
2021-08-04 23:37:24 +0530

    Printing Steps Pattern

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -20,3 +20,37 @@
 function steps(n) {}
 
 module.exports = steps;
+
+// SOLUTION 1
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let output = "";
+//     for (let col = 0; col < n; col++) {
+//       if (col <= row) {
+//         output += "#";
+//       } else {
+//         output += " ";
+//       }
+//     }
+//     console.log(output);
+//   }
+// }
+
+// SOLUTION 2
+// function steps(n, row = 0, string = "") {
+//   if (n === row) {
+//     return;
+//   }
+
+//   if (n === string.length) {
+//     console.log(string);
+//     return steps(n, row + 1);
+//   }
+
+//   if (string.length <= row) {
+//     string += "#";
+//   } else {
+//     string += " ";
+//   }
+//   steps(n, row, string);
+// }
