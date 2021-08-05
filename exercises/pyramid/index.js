a727e43cc5dad58a1f20e0584293720cdc7d15a6
Rushiraj Brahmbhatt <rbrahm812@gmail.com>
2021-08-05 22:17:58 +0530

    Pyramid solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,44 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = "") {}
 
 module.exports = pyramid;
+
+// SOLUTION 1
+// function pyramid(n) {
+//   const midPoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (midPoint - row <= col && midPoint + row >= col) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+// SOLUTION 2
+// function pyramid(n, row = 0, level = "") {
+//   if (row === n) {
+//     return;
+//   }
+
+//   if (level.length === 2 * n - 1) {
+//     console.log(level);
+//     return pyramid(n, row + 1);
+//   }
+
+//   const midPoint = Math.floor((2 * n - 1) / 2);
+//   let add;
+//   if (midPoint - row <= level.length && midPoint + row >= level.length) {
+//     add = "#";
+//   } else {
+//     add = " ";
+//   }
+
+//   pyramid(n, row, level + add);
+// }
