fa3be7b970e9ab0ad21f4b5dca80e3604b3fd6d9
Rushiraj Brahmbhatt <rbrahm812@gmail.com>
2021-09-06 00:24:23 +0530

    Selection Sort, Merge Sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -14,10 +14,42 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        let temp = arr[indexOfMin];
+        arr[indexOfMin] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
 
-function merge(left, right) {}
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  return [...result, ...left, ...right];
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
